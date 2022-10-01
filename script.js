8517a5ad5908aa3ee3396f8bdeea7c605e1e9248
Dmytro Shynkarenko <d.shynkarenko@gmail.com>
2022-10-01 20:48:49 +0300

    start project

---
+++ script.js
@@ -0,0 +1,15 @@
+let number = 5;
+const leftBorderWidth = 1;
+number =10;
+console.log(4/0);
+console.log("string" *9);
+const ka="3";
+
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+
+console.log(obj.name);
+console.log(obj["name"]);
