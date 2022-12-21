e295ce792b1ac73d4cc5398dfd538997aa716b24
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-12-21 12:56:38 +0200

    add slider v1

--- js/script.js
+++ js/script.js
@@ -224,9 +224,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const postData = async (url, data) => {
     const res = await fetch(url, {
-      metod: "POST",
+      method: "POST",
       headers: {
-        "Content-type": "aplicaation/json",
+        "Content-type": "application/json",
       },
       body: data,
     });
@@ -250,7 +250,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const object = {};
 
-      formData.forEach(function (key, value) {
+      formData.forEach((key, value) => {
         object[key] = value;
       });
 
@@ -309,6 +309,57 @@ document.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
+
+  //SLIDER
+
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    total = document.querySelector("#total"),
+    current = document.querySelector("#current");
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex); //inicialisation of slider
+
+  //counter of slides
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+  //end
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none"));
+    slides[slideIndex - 1].style.display = "block";
+
+    //slide current counter
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", () => {
+    plusSlides(1);
+  });
 
   // END SCRIPT
 });
